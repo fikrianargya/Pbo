d90d0f4c2752d4f345d3c169ecff0cb84bf200e8
fikrianargya <71370675+fikrianargya@users.noreply.github.com>
2020-10-12 23:58:45 +0700

    Add files via upload

---
+++ Return Values 1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod(3));
+    }
+}
---
+++ Values.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    static int myMethod(int x, int y){
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+	    int z = myMethod(5, 3);
+        System.out.println(z);
+    }
+}
